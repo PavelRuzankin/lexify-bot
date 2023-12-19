afb1cb53f9ee06e86232761c1e0ddddd80e2c133
PavelRuzankin <61935146+PavelRuzankin@users.noreply.github.com>
2023-12-19 21:14:31 +0400

    delete js files

--- src/session/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setupSession = void 0;
-var setup_session_1 = require("./setup-session");
-Object.defineProperty(exports, "setupSession", { enumerable: true, get: function () { return setup_session_1.setupSession; } });
